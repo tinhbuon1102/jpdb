96381e4df6b24e5105dc38e08e2824d9b9e3e2aa
thangtqvn <quocthang.2001@gmail.com>
2018-06-08 09:24:24 +0700

    Upload files

--- protected/commands/GeneratePdfCommand.php
+++ protected/commands/GeneratePdfCommand.php
@@ -26,6 +26,10 @@ class GeneratePdfCommand extends CConsoleCommand
     $snappy->setOption('margin-right', 0);
     $snappy->setOption('margin-bottom', 0);
     $url = urldecode($domain.$pdfUrl.'&print=true&zoom='.$zoom.'&zoombuilding='.$zoom_building.'&user='.$user);
+    if ($_GET['test'])
+    {
+    		echo $url;die;
+    }
     $snappy->generate($url, $images_path.'/'.$fName);
 
     if(file_exists($images_path.'/'.$fName)){
